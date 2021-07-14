65540c86e024fc9c68efd31ee18a9b82b3fe8041
showmik <77013640+Miskat-UL@users.noreply.github.com>
2021-07-14 17:34:37 +0600

    added class

--- utils/database.py
+++ utils/database.py
@@ -1,18 +1,13 @@
 import json
+from utils import account_funtionality
+
 
 file_name = 'login_data.json'
-
-def load_file():
-    with open(file_name, 'r+') as file:
-        file_content = json.load(file)
-
-    return file_content
 
 
 class JsonClass:
     def __init__(self, file):
         self.file_name = file
-
 
     def json_load(self):
         with open(self.file_name, 'r+') as file:
@@ -56,59 +51,8 @@ def login(user, password):
                 print('confirmed user name. ')
                 print('confirmed password')
                 username = _['username']
-                login_after_menu(username)
+                account_funtionality.login_after_menu(username)
                 break
     else:
         print('username/ password is incorrect')
-
-
-def login_after_menu(name):
-    print(f'welcome to our service MR. {name}')
-
-    user_choice = input("""
-        Enter A - For account Details
-        Enter D - For Deposite Money
-        Enter S - For send money
-        Enter L - Logout
-    """)
-    while user_choice != 'l':
-        if user_choice.lower() == 'a':
-            account_details(name)
-        elif user_choice == 'd':
-            deposite_money()
-        elif user_choice == 's':
-            send_money()
-        else:
-            print('Unknown command, Please try again..!')
-
-        user_choice = input("""
-                Enter A - For account Details
-                Enter D - For Deposite Money
-                Enter S - For send money
-                Enter L - Logout
-            """)
-
-
-def account_details(name):
-    for _ in files.json_load():
-        if _['username'] == name:
-            print(
-                f"""
-                username:{_['username']}
-                Email: {_['email']} 
-                """
-            )
-
-    # login_after_menu(name)
-
-
-def send_money():
-    total_tk = 1000
-    tk = int(input("Enter amount you want to sent: "))
-    print(f"your previous tk was:${total_tk} ")
-    remaining_tk = total_tk - tk
-    print(f"After send you have :${remaining_tk}")
-
-
-
 
